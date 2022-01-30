b7b9c641b31e8909d059f8b94c3e40d0698c89dd
that-fela <91596987+that-fela@users.noreply.github.com>
2022-01-30 15:52:11 +1300

    Initial commit

---
+++ ApiConnections.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_crypto_bot_4._0
+{
+    internal class ApiConnections
+    {
+        static JObject GET(string url)
+        {
+
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_crypto_bot_4._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
